93df30d71d76b6d05cf9b74b3cee772d91b2f658
Nathan Romasanta <nathanael.romasanta@edu.sait.ca>
2024-09-24 16:10:04 -0600

    Week 2 Assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+import { Heading } from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+      <StudentInfo></StudentInfo>
+    </main>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <p>Nathan Romasanta</p>
+
+      <Link href="https://github.com/nathan-romasanta?tab=repositories">
+        Github.com
+      </Link>
+    </main>
+  );
+}
